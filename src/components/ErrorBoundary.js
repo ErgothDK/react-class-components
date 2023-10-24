3143c246bbad55a51252f94fab6ea60a1824f82b
ErgothDK <ergothdk@gmail.com>
2023-10-24 00:56:10 -0600

    Added ErrorBoundary class to handling errors

---
+++ src/components/ErrorBoundary.js
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+
+class ErrorBoundary extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  componentDidCatch(error) {
+    console.log(error);
+    this.setState({ hasError: true });
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return <div className="error">Error has ocurred</div>;
+    }
+    return this.props.children;
+  }
+}
+
+export default ErrorBoundary;
